ef5f9b8ea1ff04f4420e35cc8fef5430f3f9eaf4
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 17:41:28 +0530

    Added files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Binarytree bn = new Binarytree();
+            bn.Add(30);
+            bn.Add(40);
+            bn.Add(50);
+            bn.Add(60);
+            bn.Add(80);
+                Console.WriteLine("After Preorder Result Will be: ");
+            bn.TravesrePreorder(bn.root);
+                Console.WriteLine("After Postorder Result Will be: ");
+            bn.TravesrePostorder(bn.root);
+                Console.WriteLine("After Inorder Result Will be: ");
+            bn.TravesreInorder(bn.root);
+        }
+
+    }
+}
